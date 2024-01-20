f668bd4e113ca42ad1f888272d5b348fa389dff6
aarya moktan <aaryamoktan2055@gmail.com>
2024-01-20 09:02:21 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Nevbar from './Content/Nevbar/Nevbar';
+function App() {
+  return (
+    <div >
+    <Nevbar/>
+   
+    </div>
+  );
+}
+
+export default App;
