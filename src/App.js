2c8235234473e9173c0a96743e17bf857f042678
aarya moktan <aaryamoktan2055@gmail.com>
2024-02-07 17:10:00 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Home from './Content/Home/Home';
-import Nevbar from './Content/Nevbar/Nevbar';
+
 function App() {
   return (
     <div >
-    <Nevbar/>
-    <Home/>
+   
     </div>
   );
 }
