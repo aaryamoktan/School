8f55c56a5690c779edd312f3b5409a5c5d53b764
aarya moktan <aaryamoktan2055@gmail.com>
2024-07-06 06:35:56 -0700

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Nevbar from "./Component/Nevbar"
 import Top from "./Component/Top"
 import Header from "./Component/Header"
+import Footer from "./Component/Footer"
 function App() {
   return (
     <>
     <Top/>
     <Nevbar/>
     <Header/>  
+    <Footer/>
     </>
   )
 }
--- frontend/src/Component/Footer.jsx
+++ frontend/src/Component/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <>
+            <div className=' w-full mt-40 flex justify-center gap-44'>
+                <div className=''>
+                <h1>My Aarya School</h1>
+                </div>
+                <div className='mb-10'>
+                    <h1>Quick Links</h1>
+                    <ul className='mt-10'>
+                    <li className=" cursor-pointer"><a href="#">HOME</a></li>
+          <li className="cursor-pointer mt-5 p-2 hover:font-extralight "><a href="#">ABOUT US</a></li>
+          <li className="cursor-pointer mt-5 p-2 hover:font-extralight "><a href="#">PROGRAMS</a></li>
+          <li className="cursor-pointer mt-5 p-2 hover:font-extralight "><a href="#">FACULTY</a></li>
+          <li className="cursor-pointer mt-5 p-2 hover:font-extralight "><a href="#">EVENTS & GALLERY</a></li>
+          <li className="cursor-pointer mt-5 p-2 hover:font-extralight "><a href="#">CONTACT US</a></li>
+                    </ul>
+                    
+                </div>
+                <div className=''>
+                <h1>Find Us On Map</h1>
+                    
+                </div>
+                <div className=''>
+                    <h1>Latest News</h1>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Footer
