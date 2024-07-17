7b36e7b48e51a862767e59e0c0bd9e487ab16def
aarya moktan <aaryamoktan2055@gmail.com>
2024-07-17 09:21:52 -0700

    first commit

--- frontend/src/Page/About.jsx
+++ frontend/src/Page/About.jsx
@@ -7,6 +7,45 @@ const About = () => {
         <img className=' brightness-50  w-full h-[66vh]' src="https://media.licdn.com/dms/image/C5112AQGWlbkCZPRBRw/article-cover_image-shrink_600_2000/0/1546165326053?e=2147483647&v=beta&t=7DPo6QiUgshsQv96n7Gvh1lQjdlo2l6_OjFSFcB8_MU"/>
         <p className=' w-full absolute   top-[50%]  text-white text-6xl shadow-2xl ml-auto m-auto'><p  className=' font-serif w-fit h-16 ml-[30%] '>Welcome to My School</p></p>
       </div>
+      <div className='w-full mt-[15%] brightness-50 '>
+        <h1 className='text-5xl ml-32 font-serif'>Who are We?</h1>
+        <p className=' font-serif text-lg mt-10  ml-32 w-[80%] text-cyan-100'>
+          The My International College/SS, established in 2000, has been consistent in its approach to redefine how quality education should be imparted to students in ways that do not only nurture their academic knowledge and skills but also encourage and motivate them to become better human beings. We strive to attain the challenging goal of blending competitive education with a range of extracurricular activities so that our students can excel in activities and situations that demand their personal knacks on issues that do not usually make subjects in their class rooms. We are inclined towards developing and maintaining an educational environment conducive to students with different interests and aspirations. We believe that such an approach to education is both prompt and pertinent for the overall development of students, whether through pure academic pursuits or extracurricular ambitions. There is a saying, “Everyone thinks of changing the world, but no one thinks of changing himself or herself,” and TTIC makes the students prepare to change themselves towards positive attitudes. The college has well-furnished and sophisticated rooms along with “fully equipped infrastructure." Our societies have become so competitive and globalized. Ordinary learning can’t pave the way to success in today’s world. The college molds the students academically, internationally, and, above all, spiritually to take the bull by the horns for their mesmerizing future and career. The college does not provide them only with theoretical aspects but also gives more priority to their practical aspects, taking them on field visits, conducting seminars, conferences, symposiums, and course-related workshops, along with non-credit courses for their leadership and personality development. As educators, not only for the students but also for the economic growth of the country, ensuring the “dignity and image” of the nation, students should be molded and nurtured in such a way that ascertains both the scientific as well as humanistic vision mentioned, and the college welcomes all those students, guardians, and professionals to step up to the promises of the college.
+        </p>
+      </div>
+      <div className='w-full mt-[15%] brightness-50 '>
+        <div className='mt-10'><h1 className='text-5xl ml-32 font-serif'>Mission</h1>
+        <p className='font-serif text-lg mt-5  ml-32 w-[80%] text-cyan-100'>Our Mission: Our fundamental mission is to make learning process applied, spontaneous and career-oriented to equip each student to be competent enough so as to excel in the global arena. The principal aim of this institution is to impart quality and sound education.</p></div>
+        <div className='mt-16'>
+        <h1 className='text-5xl ml-32 font-serif'>Vision</h1>
+        <p className='font-serif text-lg mt-5  ml-32 w-[80%] text-cyan-100'>Our Vision: To become a pioneer educational institution in Nepal by providing quality and time-bound educational service.</p>
+        </div>
+        <div className='mt-16'>
+        <h1 className='text-5xl ml-32 font-serif'>Objectives of the College</h1>
+        <p className='font-serif text-lg mt-5  ml-32 w-[80%] text-cyan-100'>
+        · Our Objectives: Shaping Future Leaders and Innovators At The Times International College/SS, Nepal, our primary objective is to provide a holistic education that fosters academic excellence, personal growth, and social responsibility. We are dedicated to creating an enriching learning environment that prepares our students to become leaders and innovators in their chosen fields. Key Objectives: · To develop entrepreneurship and professional managerial skills in the students. · To enable students to compete in the changing and challenging environment. · To cultivate the habit of generating innovative and creative ideas in the students through continuous encouragement. · To make learning process applied, spontaneous and career-oriented. · To equip each student to be competent enough so as to excel in the global arena.
+        </p>
+        </div>
+        <div className='mt-16'>
+        <h1 className='text-5xl ml-32 font-serif'>Eligibility for Admission</h1>
+        <p className='font-serif text-lg mt-5  ml-32 w-[80%] text-cyan-100'>The admission procedure is conducted by the college through an entrance examination followed by “Interview.” Students holding minimum of “B+” in the examinations deserve to admitted in Science stream,”B” in Management and “C” in Humanities.</p>
+        </div>
+        <div className='mt-16'>
+        <h1 className='text-5xl ml-32 font-serif'>Scholarship</h1>
+        <p className='font-serif text-lg mt-5  ml-32 w-[80%] text-cyan-100'>A wide range of scholarships are provided to academically sound, deserving, marginalized(Dalit, women, Madeshi, Janjati and Martyr’s family members) as well as the needy, but laborious students from inaccessible area who come across the criteria initiated by College Management. Limited scholarship will be provided to students of Government/public school who are found to be academically sound.</p>
+        </div>
+        <div className='mt-16'>
+        <h1 className='text-5xl ml-32 font-serif'>Personality development</h1>
+        <p className='font-serif text-lg mt-5  ml-32 w-[80%] text-cyan-100'>Personality lone can’t be developed only having academic performance to stand by in the global corporate culture , and therefore the college gives and emphasis on personality and leadership grooming through presentations, public, speaking, mock interviews, aptitude test and industry visit which help the students be pragmatic and down to earth.</p>
+        </div>
+        <div className='mt-16'>
+        <h1 className='text-5xl ml-32 font-serif'>Computer Lab facility</h1>
+        <p className='font-serif text-lg mt-5  ml-32 w-[80%] text-cyan-100'>
+        The college possesses a well-equipped computer lab with 50 LCD monitors. We have “3 multi-media facilities” for class room teaching”. We provided internet facilities. The Times International College / SS is the college that guarantees the quality of a student.It has made a commitment to recognize its students nationally and internationally. The college possesses student- friendly, attitude- oriented teaches who are always ready to help the students with their incessant efforts for which our college management is proud of.
+        </p>
+        </div>
+        
+      </div>
       </div></>
   )
 }
