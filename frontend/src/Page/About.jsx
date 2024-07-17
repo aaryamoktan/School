bd3a3e31dd1ac7aaea6d862e385293e521d660de
aarya moktan <aaryamoktan2055@gmail.com>
2024-07-17 08:58:17 -0700

    first commit

--- frontend/src/Page/About.jsx
+++ frontend/src/Page/About.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <div className=''>
+      <div className='w-full h-full mt-5  text-center'>
+        <img className=' brightness-50  w-full h-[66vh]' src="https://media.licdn.com/dms/image/C5112AQGWlbkCZPRBRw/article-cover_image-shrink_600_2000/0/1546165326053?e=2147483647&v=beta&t=7DPo6QiUgshsQv96n7Gvh1lQjdlo2l6_OjFSFcB8_MU"/>
+        <p className=' w-full absolute   top-[50%]  text-white text-6xl shadow-2xl ml-auto m-auto'><p  className=' font-serif w-fit h-16 ml-[30%] '>Welcome to My School</p></p>
+      </div>
+      </div></>
   )
 }
 
