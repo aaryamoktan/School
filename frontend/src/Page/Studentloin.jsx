c9bd1e7c9b614b4c388a1f7176681ff5cd683d56
aarya moktan <aaryamoktan2055@gmail.com>
2024-07-21 01:13:09 -0700

    first commit

--- frontend/src/Page/Studentloin.jsx
+++ frontend/src/Page/Studentloin.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Studentloin = () => {
   return (
-    <div>Studentloin</div>
+    <>
+    <div className='w-full h-[80vh]'>
+      <img className='w-full h-full brightness-50' src="https://s39613.pcdn.co/wp-content/uploads/2019/11/Implementing-active-learning-and-student-centered-pedagogy.jpg"/>
+      </div></>
   )
 }
 
