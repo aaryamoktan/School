2f6295048ad880be15dadab1915a2a55dd39c3f3
aarya moktan <aaryamoktan2055@gmail.com>
2024-07-21 05:21:50 -0700

    first commit

--- frontend/src/Page/Programs.jsx
+++ frontend/src/Page/Programs.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 const Programs = () => {
   return (
-    <div>Programs</div>
+    <>
+    <div className='w-full h-[80vh] '>
+      <img className='w-full h-[80vh] brightness-50' src="https://t3.ftcdn.net/jpg/03/01/24/58/360_F_301245840_zwJpFB1MCmJkTg1tMDK9pFnCwce6dQ1T.jpg"/>
+      <ul className=' absolute w-full list-none   top-[20%] flex gap-2 flex-col justify-center items-center text-2xl font-serif text-slate-800 '>
+        <li className='  items-center shadow-xl text-white cursor-pointer '> MONTESSORI</li>
+        <li className=' shadow-xl text-white cursor-pointer'>LKG</li>
+        <li className=' shadow-xl text-white cursor-pointer '>UKG</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 1</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 2</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 3</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 4</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 5</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 6</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 7</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 8</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 9</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS10</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 11</li>
+        <li className=' shadow-xl text-white cursor-pointer '>CLASS 12</li></ul></div></>
   )
 }
 
