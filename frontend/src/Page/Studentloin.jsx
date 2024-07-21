80d122b88d95cf08f4aa3e1001de205d6802a1b2
aarya moktan <aaryamoktan2055@gmail.com>
2024-07-21 01:34:36 -0700

    first commit

--- frontend/src/Page/Studentloin.jsx
+++ frontend/src/Page/Studentloin.jsx
@@ -4,7 +4,16 @@ const Studentloin = () => {
   return (
     <>
     <div className='w-full h-[80vh]'>
-      <img className='w-full h-full brightness-50' src="https://s39613.pcdn.co/wp-content/uploads/2019/11/Implementing-active-learning-and-student-centered-pedagogy.jpg"/>
+      <img className='w-full blur-sm  h-full brightness-50' src="https://s39613.pcdn.co/wp-content/uploads/2019/11/Implementing-active-learning-and-student-centered-pedagogy.jpg"/>
+      <div className='w-[25%] h-[50vh] shadow-2xl bg-transparent absolute top-[250px] left-[600px]'>
+        <h1 className='text-white text-3xl font-semibold font-serif ml-[100px]'>Login Form</h1>
+        <form className='flex flex-col gap-5 w-fit h-[100vh] mt-[50px] bg'>
+          <input type="text" placeholder='User_ID' className='w-[200%] h-[4vh] ml-[5px] rounded-lg'></input>
+          <input type="password" placeholder='Password' className='w-[200%] h-[4vh] ml-[5px] rounded-lg' ></input>
+          <button  className='w-[100%] bg-white h-[4vh] ml-[50%] rounded-lg'>Login</button> 
+        </form>
+        <p className='text-white absolute top-[70%] text-center font-serif text-lg'>Student Can see theri own progress and the result and other things by login with your User_Id and Password</p>
+      </div>
       </div></>
   )
 }
